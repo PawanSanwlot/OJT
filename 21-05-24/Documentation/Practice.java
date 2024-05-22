750e3bfc5c0d93b251dbe7ae901ba903ee4bb5a7
Ryker <82869753+PawanSanwlot@users.noreply.github.com>
2024-05-22 17:03:12 +0530

    Day 7 Tasks

---
+++ 21-05-24/Documentation/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+
+    public static void main(String[] main){
+        int num1 = 5;
+        int num2 = 2;
+        int total = num1+num2;
+        System.out.println(total);
+    }
+}
